c9c5167cb722e31707da8c55afcfec7bd2b014de
luketl98 <luketl98@hotmail.com>
2022-08-31 00:56:32 +0100

    update

--- hangman/hangman_solution_Final.py
+++ hangman/hangman_solution_Final.py
@@ -86,7 +86,7 @@ class Hangman:
         # TODO 3: Check if the letter is in the word. TIP: You can use the lower() method to convert the letter to lowercase
         # TODO 3: If the letter is in the word, replace the '_' in the word_guessed list with the letter
         # TODO 3: If the letter is in the word, the number of UNIQUE letters in the word that have not been guessed yet has to be reduced by 1
-        # TODO 3: If the letter is not in the word, reduce the number of lives by 1
+        # DONE -- TODO 3: If the letter is not in the word, reduce the number of lives by 1
         # Be careful! A word can contain the same letter more than once. TIP: Take a look at the index() method in the string class
 
         # Letter IS in the word
@@ -95,10 +95,13 @@ class Hangman:
                 self.word_guessed.append(letter)
                 print(self.word_guessed)
 
+        # Letter NOT in the word
         else:
                 self.list_letters.append(letter)
                 print(self.list_letters)
                 print("SHIEEEET Bitch, This letter is NOT in the word!")
+                self.num_lives = self.num_lives -1
+                print(f'You have {self.num_lives} lives left!')
 
         pass
 
@@ -123,9 +126,9 @@ class Hangman:
             #    self.word_guessed.append(letter)
             #    print(self.word_guessed)
 
-            # Letter ALREADY tried
-            elif self.list_letters.count(letter) == True:
-                print(f'This {letter} has already been tried')
+            # Checks if letter has already been tried
+            elif self.list_letters.count(letter) == True or self.word_guessed.count(letter) == True:
+                print(f'{letter} was already tried')
 
             # Letter NOT in the word
             # else:
@@ -138,18 +141,18 @@ class Hangman:
             
                 
 
-        # TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
-        # TODO 1: Assign the letter to a variable called `letter`
-        # TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character"
-        # TODO 2. It has to be a letter that has not been tried yet. Use the list_letters attribute to check this. If it has been tried, print "{letter} was already tried".
-        # TODO 3: If the letter is valid, call the check_letter method
+        # DONE -- TODO 1: Ask the user for a letter iteratively until the user enters a valid letter
+        # DONE -- TODO 1: Assign the letter to a variable called `letter`
+        # DONE -- TODO 1: The letter has to comply with the following criteria: It has to be a single character. If it is not, print "Please, enter just one character"
+        # DONE -- TODO 2. It has to be a letter that has not been tried yet. Use the list_letters attribute to check this. If it has been tried, print "{letter} was already tried".
+        # DONE -- TODO 3: If the letter is valid, call the check_letter method
             pass
         
 
 def play_game(word_list):
     # As an aid, part of the code is already provided:
     game = Hangman(word_list, num_lives=5)
-    # TODO 1: To test this task, you can call the ask_letter method
+    # DONE -- TODO 1: To test this task, you can call the ask_letter method
     game.ask_letter()
     # TODO 2: To test this task, upon initialization, two messages should be printed 
     # TODO 3: To test this task, you call the ask_letter method and check if the letter is in the word
@@ -157,6 +160,12 @@ def play_game(word_list):
     # TODO 4: Iteratively ask the user for a letter until the user guesses the word or runs out of lives
     # If the user guesses the word, print "Congratulations, you won!"
     # If the user runs out of lives, print "You ran out of lives. The word was {word}"
+
+    # CODE FOR TODO 4 BELOW ------------------------- 
+
+    # while self.num_lives <= 0:
+    #        print('You fucked it boyo')
+    #        break
 
     pass
 
